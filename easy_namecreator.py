ca3b9a059ec60c844a79cb35a99598840685f1c8
Pol Danev <123486176+PolDanev@users.noreply.github.com>
2023-02-02 23:16:32 +0200

    Easy name generator

---
+++ easy_namecreator.py
@@ -0,0 +1,8 @@
+print("Welcome to the Band-Name-Generator.")
+
+city = input("Which city did you grew up in?\n")
+
+my_pet = input("What is the name of your pet?\n")
+
+print("Your band name could be:")
+print((city) + " " + (my_pet))
