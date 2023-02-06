a9678022cb026bcd7454de7ffa33dc1b779f6851
Pol Danev <123486176+PolDanev@users.noreply.github.com>
2023-02-06 20:33:12 +0200

    Easy motivation calendar

---
+++ motivation_calendar.py
@@ -0,0 +1,11 @@
+age = input("What is your current age? ")
+
+current_age = int(age)
+
+years_remaining = 90 - current_age
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining * 12
+
+print(f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left,\nDON'T WASTE YOUR TIME!")
+
